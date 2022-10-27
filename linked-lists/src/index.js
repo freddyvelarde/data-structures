da6b0f91f4c91ef26807566644441081b719ed46
freddy velarde <freddyvelarde59@gmail.com>
2022-10-27 18:14:42 -0400

    adding new methods

--- linked-lists/src/index.js
+++ linked-lists/src/index.js
@@ -82,6 +82,97 @@ class LinkedList {
     }
     return newLinkedList;
   }
+
+  filterNodes(callback) {
+    if (typeof callback !== "function") {
+      return console.log(
+        new Error(
+          `yourLinkedList.filterNodes() must be a function and not ${typeof callback}`
+        )
+      );
+    }
+
+    let currentNode = this.head;
+    const newLinkedList = new LinkedList();
+    while (currentNode) {
+      if (callback(currentNode.value)) {
+        newLinkedList.appendNewNode(currentNode.value);
+      }
+      currentNode = currentNode.next;
+    }
+    return newLinkedList;
+  }
+
+  insertAtHead(value) {
+    const newNode = new Node(value);
+    let nextNode = this.head;
+    this.head = newNode;
+    newNode.next = nextNode;
+  }
+
+  getFirst() {
+    return this.head.value;
+  }
+
+  getLast() {
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    return currentNode.value;
+  }
+
+  findNode(callback) {
+    if (typeof callback !== "function") {
+      return console.log(
+        new Error(
+          `yourLinkedList.findNode() must be a function and not ${typeof callback}`
+        )
+      );
+    }
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (callback(currentNode.value)) {
+        return currentNode.value;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  insertAt(index, value) {
+    let node = this.head;
+    const newNode = new Node(value);
+    if (index == 0) {
+      newNode.next = node;
+      this.head = newNode;
+      return;
+    }
+    while (--index) {
+      if (node.next !== null) node = node.next;
+      else throw Error("Index Out of Bound");
+    }
+    let tempVal = node.next;
+    node.next = newNode;
+    newNode.next = tempVal;
+  }
+
+  removeFrom(index) {
+    let node = this.head;
+    if (index === 0) {
+      if (node !== null) {
+        node = node.next;
+        this.head = node;
+      } else throw Error("Index Out of Bound");
+
+      return;
+    }
+    while (--index) {
+      if (node.next !== null) node = node.next;
+      else throw Error("Index Out of Bound");
+    }
+    node.next = node.next.next;
+  }
 }
 
 export default LinkedList;
